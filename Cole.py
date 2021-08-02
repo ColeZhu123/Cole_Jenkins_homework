7d2dc8f80329550c73951aa640d6496e54dd2aaa
cole.zhu <cole.zhu@ringcentral.com>
2021-08-02 15:21:01 +0800

    123456

---
+++ Cole.py
@@ -0,0 +1,14 @@
+
+# -*- coding: UTF-8 -*-
+
+import os
+
+# Get numbers from Jenkins
+num1=os.getenv("Number1")
+num2=os.getenv("Number2")
+
+# 求和
+sum = float(num1) + float(num2)
+
+# 显示计算结果
+print('数字 {0} 和 {1} 相加结果为： {2}'.format(num1, num2, sum))
